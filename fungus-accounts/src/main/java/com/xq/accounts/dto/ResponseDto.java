5d7102967d8afc8ed200bb773ae74d15737e4b50
chauhaidang1 <chauhaidang1@gmail.com>
2024-08-25 23:26:23 +0700

    fungus-account: add customised api endpoint doc

--- fungus-accounts/src/main/java/com/xq/accounts/dto/ResponseDto.java
+++ fungus-accounts/src/main/java/com/xq/accounts/dto/ResponseDto.java
@@ -1,10 +1,15 @@
 package com.xq.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+@Schema(name = "Success response")
 @Data @AllArgsConstructor
 public class ResponseDto {
+    @Schema(example = "201")
     private String statusCode;
+
+    @Schema(example = "Account created successfully")
     private String statusMsg;
 }
