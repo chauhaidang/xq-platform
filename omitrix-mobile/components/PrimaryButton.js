967eef33e7404d6438c2724c6d09603d004148de
chauhaidang1 <chauhaidang1@gmail.com>
2024-12-10 01:03:49 +0700

    omitrix-mobile: customize button

--- omitrix-mobile/components/PrimaryButton.js
+++ omitrix-mobile/components/PrimaryButton.js
@@ -1,11 +1,42 @@
-import { Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from 'react-native'
 
 function PrimaryButton({ children }) {
-    return (
-        <View>
-            <Text>{children}</Text>
-        </View>
-    )
+  function pressHandler() {
+    console.log('Pressed!')
+  }
+
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) => (pressed ? [styles.buttonContainer, styles.pressed] : styles.buttonContainer)}
+        onPress={pressHandler}
+        android_ripple={{ color: '#ffffff' }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  )
 }
 
-export default PrimaryButton;
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden',
+  },
+  buttonContainer: {
+    backgroundColor: '#ec494c',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 10,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+})
+
+export default PrimaryButton
