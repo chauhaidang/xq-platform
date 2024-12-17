7887a466818dcb90af19eecce0af24c576e36f13
chauhaidang1 <chauhaidang1@gmail.com>
2024-12-17 23:16:41 +0700

    omitrix-mobile: add app background

--- omitrix-mobile/App.js
+++ omitrix-mobile/App.js
@@ -1,10 +1,27 @@
-import { useState } from 'react';
-import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
-import StartGameScreen from './screens/StartGameScreen';
+import { ImageBackground, StyleSheet } from 'react-native'
+import StartGameScreen from './screens/StartGameScreen'
+import { LinearGradient } from 'expo-linear-gradient'
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
+  },
+})
+
 export default function App() {
-    return (
+  return (
+    <LinearGradient colors={['#EE6B6E', '#644b64']} style={styles.rootScreen}>
+      <ImageBackground
+        source={require('./assets/images/background-image-2.jpg')}
+        resizeMode={'cover'}
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
         <StartGameScreen />
-    );
+      </ImageBackground>
+    </LinearGradient>
+  )
 }
