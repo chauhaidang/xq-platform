99e3f6fdc0098205d7a3e11e140d0eeadb5dbc16
chauhaidang1 <chauhaidang1@gmail.com>
2025-02-09 02:01:49 +0700

    omitrix-mobile: switch to npm and add react-navigation

--- omitrix-mobile/App.js
+++ omitrix-mobile/App.js
@@ -1,13 +1,21 @@
 import { StyleSheet, Text, View, StatusBar, SafeAreaView } from 'react-native'
 import CategoriesScreen from './screens/CategoriesScreen'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
     <>
       <StatusBar barStyle={'default'} />
-      <SafeAreaView style={styles.container}>
-        <CategoriesScreen />
-      </SafeAreaView>
+      {/*<SafeAreaView style={styles.container}>*/}
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Categories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      {/*</SafeAreaView>*/}
     </>
   )
 }
--- omitrix-mobile/metro.config.js
+++ omitrix-mobile/metro.config.js
@@ -1,14 +1,16 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
+const { getDefaultConfig } = require('expo/metro-config')
+const path = require('path')
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname)
 
 config.resolver = {
-    extraNodeModules: {
-        fs: path.resolve(__dirname, 'mocks/fs.js'),
-        path: path.resolve(__dirname, 'mocks/path.js'),
-    },
-};
+  extraNodeModules: {
+    fs: path.resolve(__dirname, 'mocks/fs.js'),
+    path: path.resolve(__dirname, 'mocks/path.js'),
+  },
+  // Add .cjs extension to the list of valid extensions, to fix the issue with the nanoid package
+  sourceExts: ['jsx', 'js', 'ts', 'tsx', 'cjs', 'json'],
+}
 
-module.exports = config;
+module.exports = config
