18db2d9b5e7e2bac8803b93155037fa649c77019
chauhaidang1 <chauhaidang1@gmail.com>
2025-02-16 21:38:02 +0700

    omitrix-mobile: fix ripple effect

--- omitrix-mobile/components/EntityItem.js
+++ omitrix-mobile/components/EntityItem.js
@@ -1,48 +1,53 @@
-import { Text, View, StyleSheet, Pressable } from "react-native";
-import { testProps } from "../utils/test-utils";
-import CustomColors from "../constants/colors";
+import { Text, View, StyleSheet, Pressable, Platform } from 'react-native'
+import { testProps } from '../utils/test-utils'
+import CustomColors from '../constants/colors'
 
 function EntityItem(props) {
-    const index = props.index
-    const content = props.value
-    return (
-        <View style={styles.entityItem}>
-            <Pressable
-                {...testProps(`item-entity-${index}`)}
-                android_ripple={styles.androidRipple}
-                onPress={() => {
-                    props.onDelete(props.id)
-                }}
-                style={({ pressed }) => pressed && styles.iosRipple}
-            >
-                <Text style={styles.entityItemText}>
-                    {content}
-                </Text>
-            </Pressable>
-        </View>
-    )
+  const index = props.index
+  const content = props.value
+  return (
+    <View style={styles.entityItem}>
+      <View style={styles.entityItemContainer}>
+        <Pressable
+          {...testProps(`item-entity-${index}`)}
+          android_ripple={styles.androidRipple}
+          onPress={() => {
+            props.onDelete(props.id)
+          }}
+          style={({ pressed }) => [styles.button, pressed && styles.iosRipple]}
+        >
+          <Text style={styles.entityItemText}>{content}</Text>
+        </Pressable>
+      </View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    entityItem: {
-        backgroundColor: '#fdfdfd',
-        padding: 15,
-        borderBottomWidth: 0.3,
-        borderStyle: 'solid',
-        borderColor: CustomColors.border
-    },
-    entityItemText: {
-        fontSize: 20,
-        fontFamily: 'open-sans',
-        fontStyle: 'italic',
-    },
-    androidRipple: {
-        color: CustomColors.ripple
-    },
-    iosRipple: {
-        opacity: 0.3,
-        backgroundColor: CustomColors.ripple,
-    }
+  entityItem: {
+    flex: 1,
+    marginVertical: 5,
+    padding: 10,
+  },
+  entityItemContainer: {
+    borderBottomWidth: 0.2,
+    marginHorizontal: 20,
+  },
+  entityItemText: {
+    fontSize: 24,
+    fontFamily: 'open-sans',
+    fontStyle: 'italic',
+  },
+  button: {
+    flex: 1,
+  },
+  androidRipple: {
+    color: CustomColors.ripple,
+  },
+  iosRipple: {
+    opacity: 0.5,
+    backgroundColor: CustomColors.ripple,
+  },
 })
 
 export default EntityItem
